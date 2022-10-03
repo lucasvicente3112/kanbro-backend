5e842a03868b8f5c6757239ea5832fe851ded263
Lucas Vicente <lucas.vicente@acad.pucrs.br>
2022-10-02 23:38:49 -0300

    atualizacao de quadro do time ao criar quadro

--- src/main/java/tcc/kanbro/service/QuadroService.java
+++ src/main/java/tcc/kanbro/service/QuadroService.java
@@ -2,9 +2,11 @@ package tcc.kanbro.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import tcc.kanbro.dto.QuadroDto;
 import tcc.kanbro.mapper.QuadroMapper;
 import tcc.kanbro.model.Quadro;
+import tcc.kanbro.model.Time;
 import tcc.kanbro.repository.QuadroRepository;
 import tcc.kanbro.repository.TimeRepository;
 
@@ -25,14 +27,18 @@ public class QuadroService {
     public List<QuadroDto> ListarQuadros() {
         return quadroMapper.paraListaDeQuadroDto(quadroRepository.findAll());
     }
-
+    @Transactional
     public Long cadastrar(QuadroDto quadroDto) {
+        Time timeParaAtualizar = timeRepository.findByIdTime(quadroDto.getTime());
 
         Quadro quadro = Quadro.builder()
-                .time(timeRepository.findByIdTime(quadroDto.getTime()))
+                .time(timeParaAtualizar)
                 .build();
 
         Quadro quadroSalvo = quadroRepository.save(quadro);
+        timeParaAtualizar.setQuadro(quadroSalvo);
+        timeRepository.save(timeParaAtualizar);
+
         estadoService.inicializarEstados(quadroSalvo.getIdQuadro());
         return quadroSalvo.getIdQuadro();
     }
